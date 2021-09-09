86e6fd3b385fc02d76707d1217b757f8584cd993
Danni <sjaeledyr@gmail.com>
2021-09-08 20:21:43 -0400

    Add files via upload

---
+++ WeeklySalaryCalculator/WeeklySalaryCalculator.py
@@ -0,0 +1,12 @@
+print("Welcome to Danni's Weekly Salary Calculator!")
+taxInput = input('What is your estimated Tax Rate?')
+tax = float(taxInput)
+wageInput = input('What is your current hourly wage?')
+wage = float(wageInput)
+hoursInput = input('How many hours have you worked this week?')
+hoursInput = float(hoursInput)
+grossSalary = hoursInput * wage
+estTax = grossSalary * tax
+netSalary = grossSalary - tax
+print('Your Net Salary for this week is' , netSalary, '!')
+print('Thank you!')
